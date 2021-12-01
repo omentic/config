899bbc094487396544766e68964cbc5342365531
j-james <jj@j-james.me>
2021-11-30 23:58:14 -0800

    Sweeping updates for the ThinkPad

--- setup.sh
+++ setup.sh
@@ -16,8 +16,7 @@ echo "Installing AUR helper..."
 sudo pacman -S base-devel
 git clone https://aur.archlinux.org/pakku
 cd pakku
-makepkg -s
-makepkg -i
+makepkg -si
 cd ..
 
 echo "Installing terminal packages..."
@@ -41,39 +40,39 @@ echo "Installation complete"
 echo "Configuring..."
 
 git clone https://git.sr.ht/~j-james/config
-rm -rf ~/.config
+mv ~/.config ~/.config-bak
 mv config ~/.config
 cd ~/.config
 git checkout $USER
 
 ln -s ~/.config/bash/bashrc ~/.bashrc
-ln -s ~/.config/bash/profile ~/.profile
+ln -s ~/.config/bash/profile ~/.bash_profile
 ln -s ~/.config/git/gitconfig ~/.gitconfig
-# not sure if these are necessary
 ln -s ~/.config/gtk-2.0/gtkrc-2.0 ~/.gtkrc-2.0
-sudo cp ~/.config/gtk-2.0/gtkrc-2.0 /usr/share/gtk-2.0/ # or: [...]/gtk-2.0/gtkrc
+# not sure if these are necessary
+sudo cp ~/.config/gtk-2.0/gtkrc-2.0 /usr/share/gtk-2.0/gtkrc
 sudo cp ~/.config/gtk-3.0/settings.ini /usr/share/gtk-3.0/
 
 git clone https://github.com/doctormo/GimpPS
 mkdir ~/.config/GIMP
 mv GimpPS ~/.config/GIMP/2.10
 
-sudo mkdir /usr/share/backgrounds/teal
-sudo cp ~/Pictures/backgrounds/trees.jpg /usr/share/backgrounds/teal/
-sudo cp ~/Pictures/face/compass.jpg /usr/share/backgrounds/teal/
-
 sudo mkdir /etc/X11/xorg.conf.d
 sudo cp ~/.config/xorg/00-keyboard.conf /etc/X11/xorg.conf.d/
 sudo cp ~/.config/xorg/01-touchpad.conf /etc/X11/xorg.conf.d/
+sudo cp ~/.config/xorg/20-intel.conf /etc/X11/xorg.conf.d/
 
 sudo chmod a+wr /opt/spotify
 sudo chmod a+wr /opt/spotify/Apps -R
 
 # in /etc/pacman.conf under Misc options, uncomment Color and append ILoveCandy
-# launch "LightDM GTK+ Greeter settings" to customize the logon screen
 # set up timeshift backups with the GUI application
 # run spicetify backup and spicetify apply
-# restore chromium profile
+# restore firefox profile
 # reinstall code extensions
 
 echo "Configuration complete"
+
+# /usr/share/icons/default/index.theme change to breeze
+
+# all the contents of ~/.local/share/applications
